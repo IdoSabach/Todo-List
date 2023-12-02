4e24947e1fc95e075cc2e13200b2165e5d103606
Ido <Idosabach11@gmail.com>
2023-12-02 22:49:37 +0200

    add load page

--- src/UI.js
+++ src/UI.js
@@ -1,32 +1,32 @@
-import { todoList , addNewProject , addNewTask } from "./strong";
+import { todoList, addNewProject, addNewTask } from "./strong";
 
 export default function loadHomePage() {
   addTaskOnInbox();
-  addNewProjectToAside()
+  addNewProjectToAside();
+  loadMainPage();
 }
-
 
 function addTaskOnInbox() {
   const addTaskBtn = document.querySelector(".add-task");
   const mainBoxOfPage = document.querySelector(".main-page");
   addTaskBtn.addEventListener("click", function () {
     addTaskBtn.style.display = "none";
-    mainBoxOfPage.appendChild(boxOfCreateTasks(addTaskBtn,"task"));
-    returnToAddTask(mainBoxOfPage, addTaskBtn);
+    mainBoxOfPage.appendChild(boxOfCreateTasks(addTaskBtn, "task"));
+    addFatherWithChild(mainBoxOfPage, addTaskBtn);
   });
 }
 
-function addNewProjectToAside(){
+function addNewProjectToAside() {
   const addProjectBtn = document.querySelector(".addBtn");
   const mainBoxOfProject = document.querySelector(".mainProject");
-  addProjectBtn.addEventListener('click',function(){
+  addProjectBtn.addEventListener("click", function () {
     addProjectBtn.style.display = "none";
-    mainBoxOfProject.appendChild(boxOfCreateTasks(addProjectBtn,"project"));
-    returnToAddTask(mainBoxOfProject , addProjectBtn);
-  })
+    mainBoxOfProject.appendChild(boxOfCreateTasks(addProjectBtn, "project"));
+    addFatherWithChild(mainBoxOfProject, addProjectBtn);
+  });
 }
 
-function boxOfCreateTasks(displayBox,type) {
+function boxOfCreateTasks(displayBox, type) {
   const mainBox = document.createElement("div");
   mainBox.classList.add("mainBoxOfCreateTask");
 
@@ -50,16 +50,15 @@ function boxOfCreateTasks(displayBox,type) {
     if (inputText.value === "") {
       alert("Enter your text please");
     } else {
-      createLineOfTask(mainBox, inputText.value);
+      createLineOfTask(mainBox, inputText.value, type);
       mainBox.removeChild(mainForBtn);
       mainBox.removeChild(inputText);
       displayBox.style.display = "flex";
-      if(type === "task"){
+      if (type === "task") {
         addNewTask("inbox", inputText.value);
-      }else{
-        addNewProject(inputText.value)
+      } else {
+        addNewProject(inputText.value);
       }
-      
     }
   });
 
@@ -78,32 +77,56 @@ function boxOfCreateTasks(displayBox,type) {
   return mainBox;
 }
 
-function returnToAddTask(father, child) {
+function addFatherWithChild(father, child) {
   father.appendChild(child);
 }
 
-function createLineOfTask(main, text) {
-  const taskLine = document.createElement("div");
-  taskLine.classList.add("taskLine");
+function createLineOfTask(main, text, type) {
+  if (type === "task") {
+    const taskLine = document.createElement("div");
+    taskLine.classList.add("taskLine");
 
-  const textTask = document.createElement("div");
-  textTask.classList.add("textTask");
-  textTask.textContent = text;
-  taskLine.appendChild(textTask);
+    const textTask = document.createElement("div");
+    textTask.classList.add("textTask");
+    textTask.textContent = text;
+    taskLine.appendChild(textTask);
 
-  const closeIconBtn = document.createElement("button");
-  closeIconBtn.classList.add("closeIconBtn");
+    const closeIconBtn = document.createElement("button");
+    closeIconBtn.classList.add("closeIconBtn");
 
-  const closeIcon = document.createElement("span");
-  closeIcon.classList.add("material-symbols-outlined");
-  closeIcon.innerHTML = "close";
+    const closeIcon = document.createElement("span");
+    closeIcon.classList.add("material-symbols-outlined");
+    closeIcon.innerHTML = "close";
 
-  closeIconBtn.appendChild(closeIcon);
-  textTask.appendChild(closeIconBtn);
+    closeIconBtn.appendChild(closeIcon);
+    textTask.appendChild(closeIconBtn);
 
-  closeIconBtn.addEventListener("click", function () {
-    main.removeChild(taskLine);
+    closeIconBtn.addEventListener("click", function () {
+      main.removeChild(taskLine);
+    });
+
+    main.appendChild(taskLine);
+  }
+}
+
+function loadMainPage() {
+  const projectBtn = document.querySelectorAll(".boxBtn");
+  const mainPage = document.querySelector(".main-page");
+  const titleOfPage = document.querySelector(".title-of-main");
+  const addTaskBtn = document.querySelector(".add-task");
+  projectBtn.forEach((button) => {
+    button.addEventListener("click", function () {
+      const type = button.dataset.type;
+      mainPage.appendChild(addTaskBtn);
+      if (type === "Today" || type === "This Week") {
+        mainPage.removeChild(addTaskBtn);
+      }
+      titleOfPage.textContent = type;
+      loadLocalStorage(type);
+    });
   });
+}
 
-  main.appendChild(taskLine);
+function loadLocalStorage(nameProject) {
+  // print all task of project
 }
